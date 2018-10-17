aa0d29c1cd381c74fae291e3910cd034ff68fe9c
nanaDio <zhenzhong.fu@gmail.com>
2018-10-17 17:49:33 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,16 +1,4 @@
 package main
 
-import (
-	"toy/hello" // import hello包
-	//"toy_util"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	//result := toy_util.Add(1, 2)
-	//hello.Print(result)
-	hello.Print(3)
-
-	gin.New()
 }
